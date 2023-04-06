cf5da974e19c61a0243492d8d0ecc64e058093e5
nacho <nacho.mota@gmail.com>
2023-04-06 11:59:55 +0200

    ctrl windows OK

--- sources/src/main.c
+++ sources/src/main.c
@@ -6,24 +6,15 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 11:55:25 by nacho             #+#    #+#             */
-/*   Updated: 2023/04/06 11:32:06 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/04/06 11:59:08 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../mlx/mlx.h"
+#include "push_swap.h"
 #include "push_swap_game.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-// int	mouse_press(int button, int x, int y, t_game *g)
-// {
-// 	printf(CYAN "x:%d, y:%d\n", x, y);
-// 	(void)g;
-// 	(void)button;
-// 	if (x >= 57 && x <= 127 && y >= 429 && y <= 495)
-// 		mlx_put_image_to_window(g->mlx, g->win, g->img.btt[SA][DOWN], 52, 462);
-// 	return (0);
-// }
 
 void	key_hook(int keycode, t_game *game)
 {
@@ -42,10 +33,20 @@ void	put_wall(t_game *g)
 	mlx_put_image_to_window(g->mlx, g->win, g->img.wall, 0, 0);
 }
 
+void	ctrl_win(t_game *g)
+{
+	mlx_hook(g->win, ON_DESTROY, 1L << 0, (void *)exit, g);
+	mlx_hook(g->win, ON_MOUSEDOWN, 1L << 2, (void *)mouse_down, g);
+	mlx_hook(g->win, ON_MOUSEUP, 1L << 1, (void *)mouse_up, g);
+	mlx_key_hook(g->win, (void *)key_hook, g);
+}
 int	main(void)
 {
 	t_game	g;
 
+	// t_data	data;
+	// t_stack	*stack_a;
+	// t_stack	*stack_b;
 	g.mlx = mlx_init();
 	g.win = mlx_new_window(g.mlx, 560, 560, "nach131 Push Swap");
 	init_img_btt(&g);
@@ -53,11 +54,7 @@ int	main(void)
 	put_wall(&g);
 	put_chip(&g);
 	mlx_put_image_to_window(g.mlx, g.win, g.img.mov, 230, 7);
-	mlx_hook(g.win, ON_DESTROY, 1L << 0, (void *)exit, &g);
-	mlx_hook(g.win, ON_MOUSEDOWN, 1L << 2, (void *)mouse_down, &g);
-	mlx_hook(g.win, ON_MOUSEUP, 1L << 1, (void *)mouse_up, &g);
-	mlx_key_hook(g.win, (void *)key_hook, &g);
-	// mlx_mouse_hook(g.win, mouse_press, &game);
+	ctrl_win(&g);
 	mlx_loop(g.mlx);
 	return (0);
 }
